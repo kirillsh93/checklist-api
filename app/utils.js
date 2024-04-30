d4627308c72797b79fc5e3925b864296ae5531bb
Kirill Shapoval <kirill.shapoval@gmail.com>
2024-04-30 16:05:47 +0300

    Add Prisma; Setup basic CRUD, pagination, sorting;

---
+++ app/utils.js
@@ -0,0 +1,24 @@
+import { configuration } from './config.js';
+
+const { pagination, order } = configuration;
+
+export const parseSortParams = ({
+  fields = [],
+  direction = order.direction,
+  orderBy = order.orderBy,
+}) => ({
+  orderBy: fields.includes(orderBy) ? orderBy : order.orderBy,
+  direction: order.options.includes(direction) ? direction : order.direction,
+});
+
+export const parsePaginationParams = ({
+  limit = pagination.limit,
+  offset = pagination.offset,
+}) => ({
+  limit: !Number.isNaN(Number.parseInt(limit))
+    ? Number.parseInt(limit)
+    : pagination.limit,
+  offset: !Number.isNaN(Number.parseInt(offset))
+    ? Number.parseInt(offset)
+    : pagination.offset,
+});
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import http from 'node:http';
 
 import { configuration } from './app/config.js';
 import { app } from './app/index.js';
+import { connect } from './app/database.js';
 
 const { port } = configuration.server;
+
+// Connect to the database
+connect();
 
 const server = http.createServer(app);
 
